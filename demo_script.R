79eff61df7e0f8caada512ac5615b227a58989a3
hakon-teaching <haakon.otneim@protonmail.com>
2023-08-25 13:40:59 +0200

    test

--- demo_script.R
+++ demo_script.R
@@ -1,6 +1,13 @@
 # Headline
 
-x <- rnorm(100)
-y <- rnorm(100)
+x <- rnorm(400)  # Bugfix in fork
+y <- rnorm(500)
 
 # Important feature
+
+# An alternative way forward
+# New feature - adding something
+# Some more work for github
+# Offline edit
+
+
